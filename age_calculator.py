453e39d5777682c302c96d8cef0b87a3a9f0d45e
AmanKumarReddy <amankumarreddy2607@gmail.com>
2025-08-04 19:23:46 +0530

    Create age_calculator.py

---
+++ age_calculator.py
@@ -0,0 +1,37 @@
+while True:
+    
+    print("============enter your birthday dates============\n")
+    
+    r = int(input("enter your born year: "))
+    m = int(input("enter the month number: "))
+    d = int(input("enter the date of your day: "))
+    
+    o = input(f"\ncheck once again! Your Date of birth is {d}/{m}/{r}. Is it correct? (yes/no): \n")
+    
+    if o.lower() == "no":
+        print("\nthen plz re-enter again...........\n ")
+    elif o.lower() == "yes":
+        
+        print("\n===========enter the current dates======  \n")
+        
+        ar = int(input("now enter the present year: "))
+        mo = int(input("enter the present month: "))
+        da = int(input("and enter the present day: "))
+        break
+
+# After confirmation, continue calculation
+D = 30
+if da < d:
+    da += D
+    mo -= 1
+
+if mo < m:
+    mo += 12
+    ar -= 1
+
+years = ar - r
+months = mo - m
+days = da - d
+
+print("\n\n")
+print("so finally you are", years, "years +", months, "months and +", days, "days old")
